b15825024e9fc2c2118770eebd5b8657db67e057
Richard Baguley <20583399+bagg3rs@users.noreply.github.com>
2022-05-27 12:45:06 +0100

    :memo: Add social things

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/react';
-
+import { SocialIcon } from 'react-social-icons';
 const HomePageStyle = css`
   h1 {
     font-size: 5rem;
@@ -14,9 +14,11 @@ const HomePage = () => {
     <div css={[HomePageStyle]}>
       <h1 className="title">richardbaguley.com</h1>
       <p>This is Richard Baguley's 🏠 🔨 📝.</p>
-      <p> </p>
+      <p>Here are my GitHub stats:</p>
       <img align="center" src="https://github-readme-stats.vercel.app/api?username=bagg3rs&count_private=true&show_icons=true&hide_title=true&hide=issues" />
-        <br />
+      <p> 
+        <SocialIcon url="https://uk.linkedin.com/in/rbaguley" /> <SocialIcon url="https://github.com/bagg3rs" />
+      </p>
     </div>
   );
 };
